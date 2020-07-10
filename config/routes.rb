dafc757945ac623813d0e11965550dfbe6b5977e
vagrant <vagrant@localhost.localdomain>
2020-07-10 13:04:26 +0000

    add footer

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root "homes#top"
   get "home/about" => "homes#about"
